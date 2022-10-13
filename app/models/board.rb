bf440b243fb5cfd50e7f7dcdb6d78cc072a07983
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-10-13 17:03:57 +0530

    generated list and item models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ApplicationRecord
+  validates :name, presence: true
+  has_many :lists, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_092320) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_112729) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.string "title"
+    t.string "class_list"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  add_foreign_key "lists", "boards"
 end
